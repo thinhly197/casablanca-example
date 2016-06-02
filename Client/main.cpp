4e5042704d1f01b6ba72d316bdbcea7a070a2695
Thinh Hung Ly <thinh.ly@ascendcorp.com>
2016-06-02 11:21:10 +0700

    Initial Commit

---
+++ Client/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Client
+//
+//  Created by Thinh Hung Ly on 6/2/16.
+//  Copyright © 2016 Thinh Hung Ly. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
